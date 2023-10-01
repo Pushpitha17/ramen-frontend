fc2fbcd9690a505d6e17774fc6cbe4cc7e1b99c9
desilvakdn <desilvakdn2@gmail.com>
2023-10-01 10:49:19 +0530

    Test 1

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,34 +1,34 @@
-import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const TopBar = () => {
   return (
-    <AppBar position="static" >
+    <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-        <Typography
+          <Typography
             variant="h6"
             noWrap
             component="a"
             href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            Project Ramen
+            Project Ramen Hello
           </Typography>
-          </Toolbar>
-       </Container>
+        </Toolbar>
+      </Container>
     </AppBar>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
